bc27876c24305d7cd9f4ff590f608608b294a286
DanliaQwerty20 <danlia2000789@gmail.com>
2026-05-03 01:26:43 +0300

    Initial commit

---
+++ src/main/java/by/system/gethired/GethiredApplication.java
@@ -0,0 +1,13 @@
+package by.system.gethired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GethiredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GethiredApplication.class, args);
+    }
+
+}
